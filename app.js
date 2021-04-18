8471448d4e599cca681f43315a02433960985c5d
Aarushkumartiwari <79394257+Aarushkumartiwari@users.noreply.github.com>
2021-04-18 09:11:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+var cartvalue =document.getElementById("cart-value") ;
+var cartbutton=document.getElementById("cart") ;
+
+var book1add=document.getElementById("book 1") ;
+var book2add=document.getElementById("book 2") ;
+var book3add=document.getElementById("book 3") ;
+var book4add=document.getElementById("book 4") ;
+var book5add=document.getElementById("book 5") ;
+var book6add=document.getElementById("book 6") ;
+
+var game1add=document.getElementById("game 1") ;
+var game2add=document.getElementById("game 3") ;
+var game3add=document.getElementById("game 3") ;
+var game4add=document.getElementById("game 4") ;
+
+var craft1add=document.getElementById("craft 1") ;
+var craft2add=document.getElementById("craft 2") ;
+var craft3add=document.getElementById("craft 3") ;
+var craft4add=document.getElementById("craft 4") ;
+
+
+
+
+
